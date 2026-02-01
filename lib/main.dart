9807535828a3fd65a906e7ee08533a2902541cb1
Abdelrhman Ashraf <amm280206@gmail.com>
2026-02-01 03:29:34 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:bookly_app/features/splach/presentation/views/splach_view.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const BooklyApp());
+}
+
+class BooklyApp extends StatelessWidget {
+  const BooklyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp(
+      home: SplachView(),
+    );
+  
+  }
+}
+
