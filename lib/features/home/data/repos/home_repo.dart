e281a9ef63b194a37150954e3c3afb14d23216ce
Abdelrhman Ashraf <amm280206@gmail.com>
2026-03-13 22:03:42 +0200

    Refactor book model structure and add error handling; implement BookModel and Failures classes

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -1,4 +1,8 @@
+import 'package:bookly_app/core/errors/failurs.dart';
+import 'package:bookly_app/features/home/data/models/books_model/book_model.dart';
+import 'package:dartz/dartz.dart';
+
 abstract class HomeRepo {
-  fetchBestSellerBooks();
-  fetchfeaturedBooks();
+ Future<Either<Failures, List<BookModel>>> fetchBestSellerBooks();
+ Future<Either<Failures, List<BookModel>>> fetchfeaturedBooks();
 }
