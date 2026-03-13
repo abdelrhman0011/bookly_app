d01e6c1984c4e80f03552b3f39286b86581d397a
Abdelrhman Ashraf <amm280206@gmail.com>
2026-03-13 04:11:23 +0200

    Refactor BookDetailsViewBody to use BooksDetailsSection and SimilarBooksSection; add new widgets for better structure

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,8 +1,6 @@
-import 'package:bookly_app/core/utils/styles.dart';
-import 'package:bookly_app/features/home/presentation/views/widgets/book_rate.dart';
-import 'package:bookly_app/features/home/presentation/views/widgets/books_action.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/books_details_section.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_details_appbar.dart';
-import 'package:bookly_app/features/home/presentation/views/widgets/custom_listview_item.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/similar_books_section.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -10,35 +8,28 @@ class BookDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var width = MediaQuery.of(context).size.width;
-    return Padding(
+    
+    return CustomScrollView(
+slivers: [
+  SliverFillRemaining(
+    hasScrollBody: false,
+    child:Padding(
       padding: const EdgeInsets.symmetric(horizontal: 25.0),
       child: Column(
         children: [
           CustomBookDetailesAppBar(),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: width*0.27),
-            child: FeatuerdListViewItem(),
-          ),
-          SizedBox(height: 43,),
-          Text("The Jungle Book",
-          style: Styles.textstyle30.copyWith(fontWeight: FontWeight.bold),
-          ),
-          SizedBox(height: 6,),
-          Text("Rudyard Kipling",
-          style: Styles.textstyle18.copyWith(color: Colors.grey),
-         
+         BooksDetailsSection(),
+          SizedBox(height: 50,),
+          SimilarBooksSection(),
 
-          ),
-          SizedBox(height: 15,),
-          BookRating(
-            mainAxisAlignment: MainAxisAlignment.center,
-          ),
-          SizedBox(height: 37,),
-          BooksAction(),
         ],
       ),
+    ) ,
+  )
+],
     );
+      
+     
   }
 }
 
---
+++ lib/features/home/presentation/views/widgets/books_details_section.dart
@@ -0,0 +1,39 @@
+import 'package:bookly_app/core/utils/styles.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/book_rate.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/books_action.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/custom_listview_item.dart';
+import 'package:flutter/material.dart';
+
+class BooksDetailsSection extends StatelessWidget {
+  const BooksDetailsSection({super.key});
+
+  @override
+  
+  Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
+    return Column(
+      children: [
+        Padding(
+            padding: EdgeInsets.symmetric(horizontal: width*0.27),
+            child: FeatuerdListViewItem(),
+          ),
+          SizedBox(height: 43,),
+          Text("The Jungle Book",
+          style: Styles.textstyle30.copyWith(fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 6,),
+          Text("Rudyard Kipling",
+          style: Styles.textstyle18.copyWith(color: Colors.grey),
+         
+
+          ),
+          SizedBox(height: 15,),
+          BookRating(
+            mainAxisAlignment: MainAxisAlignment.center,
+          ),
+          SizedBox(height: 37,),
+          BooksAction(),
+      ],
+    );
+  }
+}
