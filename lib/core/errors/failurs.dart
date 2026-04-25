c0633a5ae39c73f2c6cefa963bb33af3ca243811
Abdelrhman Ashraf <amm280206@gmail.com>
2026-04-26 02:54:26 +0300

    Refactor error handling in Failures class and update HomeRepoImpl to fetch best seller books with improved error management

--- lib/core/errors/failurs.dart
+++ lib/core/errors/failurs.dart
@@ -1,3 +1,54 @@
-abstract class Failures {}
+import 'package:dio/dio.dart';
 
-class ServerFailure extends Failures {}
+
+abstract class Failures {
+  final String errMessage;
+
+  Failures(this.errMessage);
+}
+
+class ServerFailure extends Failures {
+  ServerFailure(super.errMessage);
+
+  factory ServerFailure.fromDioError(DioException dioError) {
+    switch (dioError.type) {
+      case DioExceptionType.connectionTimeout:
+        return ServerFailure('Connection timeout');
+
+      case DioExceptionType.sendTimeout:
+        return ServerFailure('Send timeout');
+
+      case DioExceptionType.receiveTimeout:
+        return ServerFailure('Receive timeout');
+
+      case DioExceptionType.badResponse:
+        return ServerFailure.fromResponse(dioError.response!.statusCode!, dioError.response!.data);
+
+      case DioExceptionType.cancel:
+        return ServerFailure('Request was cancelled');
+
+      case DioExceptionType.unknown:
+        return ServerFailure(
+          'An unknown error occurred: ${dioError.message}',
+        );
+
+      case DioExceptionType.badCertificate:
+        return ServerFailure('Bad certificate (SSL issue)');
+
+      case DioExceptionType.connectionError:
+        return ServerFailure('Connection error, check your internet');
+      default:
+        return ServerFailure('An error occurred, try later');}
+  }
+ factory ServerFailure.fromResponse(int statusCode , dynamic response){
+  if (statusCode == 400 || statusCode == 401 || statusCode == 403) {
+    return ServerFailure(response['error']['message']);
+  }else if (statusCode == 404) {
+    return ServerFailure(' you request Not found, try later');
+  } else if (statusCode == 500) {
+    return ServerFailure('Internal Server Error, try later');
+  } else {
+    return ServerFailure('An error occurred, try later');
+  }
+ }
+}
--- lib/features/home/data/repos/home_repo_implemnt.dart
+++ lib/features/home/data/repos/home_repo_implemnt.dart
@@ -1,13 +1,31 @@
 import 'package:bookly_app/core/errors/failurs.dart';
+import 'package:bookly_app/core/utils/api_service.dart';
 import 'package:bookly_app/features/home/data/models/books_model/book_model.dart';
 import 'package:bookly_app/features/home/data/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 class HomeRepoImpl implements HomeRepo {
+  final ApiService apiService;
+
+  HomeRepoImpl(this.apiService);
   @override
-  Future<Either<Failures, List<BookModel>>> fetchBestSellerBooks() {
-    // TODO: implement fetchBestSellerBooks
-    throw UnimplementedError();
+  Future<Either<Failures, List<BookModel>>> fetchBestSellerBooks() async {
+ try {
+  var data =await apiService.get(endpoint: "volumes?q=subject :comics&filter=free-ebooks&key=AIzaSyDW53SaBNSUh66Cp7Aq1ui66s4uKfinW5g&Sorting=newest");
+  List<BookModel> book =[];
+  for (var item in data['items']) 
+  {
+      book.add(BookModel.fromJson(item));  
+  }
+  return right(book);
+} catch (e) {
+  if (e is DioException) {
+    return left(ServerFailure.fromDioError(e));
+  }
+  return left(ServerFailure(e.toString()));
+  
+}
   }
 
   @override
