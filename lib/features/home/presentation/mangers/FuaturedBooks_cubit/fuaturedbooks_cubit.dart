98a1fc338dfb61c97082e44b8c375598a302c055
Abdelrhman Ashraf <amm280206@gmail.com>
2026-04-26 03:15:38 +0300

    Implement FuaturedbooksCubit with fetchFuaturedBooks method and state management

--- lib/features/home/presentation/mangers/FuaturedBooks_cubit/fuaturedbooks_cubit.dart
+++ lib/features/home/presentation/mangers/FuaturedBooks_cubit/fuaturedbooks_cubit.dart
@@ -1,9 +1,20 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly_app/features/home/data/models/books_model/book_model.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'fuaturedbooks_state.dart';
 
 class FuaturedbooksCubit extends Cubit<FuaturedbooksState> {
-  FuaturedbooksCubit() : super(FuaturedbooksInitial());
+  FuaturedbooksCubit(this.homeRepo) : super(FuaturedbooksInitial());
+  final HomeRepo homeRepo;
+  Future<void> fetchFuaturedBooks() async {
+    emit(FuaturedbooksLoading());
+    var result = await homeRepo.fetchfeaturedBooks();
+    result.fold((failure) {
+      emit(FuaturedbooksError(failure.errMessage));
+    }, (books) {
+      emit(FuaturedbooksSuccess(books));
+    });
+  }
 }
